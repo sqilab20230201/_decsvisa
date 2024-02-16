cc631a56905faf561d65d4427fd5da9ecad6291a
abioxinst <128148810+abioxinst@users.noreply.github.com>
2024-02-16 10:03:22 +0000

    decsvisa.log added for Windows

--- src/decs_visa_tools/base_logger.py
+++ src/decs_visa_tools/base_logger.py
@@ -1,19 +1,27 @@
 """
 Logging module
 """
-
+ 
 import logging
-
+import platform
+ 
 # Set up logs
 logger = logging.getLogger(__name__)
-
+ 
 # Log everything / something
 #logger.setLevel(logging.DEBUG)
 logger.setLevel(logging.INFO)
-
-# For now, log to console
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-logger.addHandler(ch)
+ 
+running_on = platform.platform()
+if running_on.startswith("Windows"):
+    fh = logging.FileHandler('decs_visa.log')
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+else:
+    # For now, log to console (to allow PIPEd output)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
