88ed17815517558208451d1043daa950eebbae85
abioxinst <128148810+abioxinst@users.noreply.github.com>
2024-02-16 10:05:37 +0000

    required user configuration made clearer

--- src/decs_visa_tools/decs_visa_settings.py
+++ src/decs_visa_tools/decs_visa_settings.py
@@ -2,14 +2,20 @@
 Utility module that holds some useful settings
 """
 
-# required for features such as match/case
-PYTHON_MIN_MAJOR = 3
-PYTHON_MIN_MINOR = 10
+#############################################
+#    Configuration settings required     #
+#############################################
 
 # path to the system settings .env file
 DOT_ENV_PATH = ".env"
 # NB this can also be an
 # /absolute/path/to/file/.env
+
+##############################################
+
+# required for features such as match/case
+PYTHON_MIN_MAJOR = 3
+PYTHON_MIN_MINOR = 10
 
 # Connection details for the socket_server
 # These values should be consistent with
