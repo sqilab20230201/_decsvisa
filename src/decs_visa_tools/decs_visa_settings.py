f4c3c77377bf3f45ce9b5e44ed5d1cdc8f3b8dee
Tony <anthony.matthews@oxinst.com>
2024-03-08 19:35:29 +0000

    merge incoming change

--- src/decs_visa_tools/decs_visa_settings.py
+++ src/decs_visa_tools/decs_visa_settings.py
@@ -7,14 +7,9 @@ Utility module that holds some useful settings
 #############################################
 
 # path to the system settings .env file
-<<<<<<< HEAD
-#DOT_ENV_PATH = "../../src/qcodes_contrib_drivers/drivers/OxfordInstruments/decsvisa/src/.env"
-DOT_ENV_PATH = "./.env"
-=======
 DOT_ENV_PATH = "./.env"
 #DOT_ENV_PATH = "../../src/qcodes_contrib_drivers/drivers/OxfordInstruments/decsvisa/src/.env"
 
->>>>>>> 3c87d4e5b33fbd451f515de5fe03b9da91de9a2e
 # NB this can also be an
 # /absolute/path/to/file/.env
 
